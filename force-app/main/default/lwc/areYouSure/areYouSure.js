f653794894a1cff676f76212efb397c5ad8f405a
Praenei <32510807+Praenei@users.noreply.github.com>
2024-09-03 14:27:33 +0100

    Add files via upload

--- force-app/main/default/lwc/areYouSure/areYouSure.js
+++ force-app/main/default/lwc/areYouSure/areYouSure.js
@@ -1,1 +1,20 @@
+import { api } from 'lwc';
+import LightningModal from 'lightning/modal';
 
+export default class AreYouSure extends LightningModal {  
+
+    @api message;
+
+    handleApplyClose() {
+        this.close(false);
+    }
+
+    handleContinue() {
+        this.close(true);
+    }
+
+    handleCancel() {
+        this.close(false);
+    }
+
+}
